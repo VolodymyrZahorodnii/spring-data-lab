b369bab5c4187ab066451899ec40f5c6ba5b2f17
VolodymyrZahorodnii <zagorodniy.vovik@gmail.com>
2024-03-16 18:52:33 +0200

    One last push, major updates were made

--- src/main/kotlin/ua/kpi/its/lab/data/svc/impl/EntityServiceImpl.kt
+++ src/main/kotlin/ua/kpi/its/lab/data/svc/impl/EntityServiceImpl.kt
@@ -2,11 +2,89 @@ package ua.kpi.its.lab.data.svc.impl
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import ua.kpi.its.lab.data.svc.EntityService
+import ua.kpi.its.lab.data.entity.Satellite
+import ua.kpi.its.lab.data.entity.Processor
+import ua.kpi.its.lab.data.repo.SatelliteRepository
+import ua.kpi.its.lab.data.repo.ProcessorRepository
+import ua.kpi.its.lab.data.svc.SatelliteService
+import ua.kpi.its.lab.data.svc.ProcessorService
 
 @Service
-class EntityServiceImpl @Autowired constructor(
-    // Your code here
-): EntityService {
-    // Your code here
+class SatelliteServiceImpl @Autowired constructor(
+    private val repository: SatelliteRepository
+): SatelliteService {
+    override fun create(satellite: Satellite): Satellite {
+        if (satellite.id != -1L && repository.existsById(satellite.id)) {
+            throw IllegalArgumentException("Satellite with ID = ${satellite.id} already exists")
+        }
+        return repository.save(satellite)
+    }
+
+    override fun read(): List<Satellite> {
+        return repository.findAll()
+    }
+
+    override fun readByIndex(index: Int): Satellite {
+        return this.read()[index]
+    }
+
+    override fun update(satellite: Satellite): Satellite {
+        if (!repository.existsById(satellite.id)) {
+            throw IllegalArgumentException("Satellite with ID = ${satellite.id} not found")
+        }
+        return repository.save(satellite)
+    }
+
+    override fun delete(satellite: Satellite) {
+        if (!repository.existsById(satellite.id)) {
+            throw IllegalArgumentException("Satellite with ID = ${satellite.id} not found")
+        }
+        repository.deleteById(satellite.id)
+    }
+
+    override fun deleteByIndex(index: Int): Satellite {
+        val target = this.readByIndex(index)
+        this.delete(target)
+        return target
+    }
+}
+
+@Service
+class ProcessorServiceImpl @Autowired constructor(
+    private val repository: ProcessorRepository
+): ProcessorService {
+    override fun create(processor: Processor): Processor {
+        if (processor.id != -1L && repository.existsById(processor.id)) {
+            throw IllegalArgumentException("Processor with ID = ${processor.id} already exists")
+        }
+        return repository.save(processor)
+    }
+
+    override fun read(): List<Processor> {
+        return repository.findAll()
+    }
+
+    override fun readByIndex(index: Int): Processor {
+        return this.read()[index]
+    }
+
+    override fun update(processor: Processor): Processor {
+        if (!repository.existsById(processor.id)) {
+            throw IllegalArgumentException("Processor with ID = ${processor.id} not found")
+        }
+        return repository.save(processor)
+    }
+
+    override fun delete(processor: Processor) {
+        if (!repository.existsById(processor.id)) {
+            throw IllegalArgumentException("Processor with ID = ${processor.id} not found")
+        }
+        repository.deleteById(processor.id)
+    }
+
+    override fun deleteByIndex(index: Int): Processor {
+        val target = this.readByIndex(index)
+        this.delete(target)
+        return target
+    }
 }
